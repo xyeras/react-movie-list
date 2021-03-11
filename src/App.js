10d6a86e9058b4195d151e89481d98056918c5d6
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-03-11 13:31:52 -0800

    added upon the skeleton

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import from './MovieList'
 import './App.css';
 
 const App = () => {
+  
   return (
     <div>
       <MovieList movies={movies}/>
     </div>
   );
+
 }
 
 export default App;
--- src/MovieList.js
+++ src/MovieList.js
@@ -40,3 +40,40 @@ let movies = [
         imdbRating: 6.6
     }
 ];
+
+const MovieList = ({movies}) => {
+
+    const showDetails = movieId => {
+        movies.find(movie => movie.id === movieId);
+
+        if (!foundMovie) {
+            alert(`Sorry! Could not find your hero!`);
+        } else {
+            alert(`${foundMovie}: ${foundMovie.description}`)
+        }
+
+    };
+
+    return (
+        <div className="row">
+            <div className="col-6 offset-3">
+                <ul className='list-group'>
+                    
+                    {movies.map((hero, index) => {
+                        return (
+                        <li className='list-group-item' key={index}>
+                            <a
+                            href='javascript:void(0)'
+                            onClick={() => showDetails(movie.id)}>
+                            {movie.name}
+                            </a>
+                        </li>
+                        );
+                    })}
+
+                </ul>
+            </div>
+        </div>
+    );
+
+};
